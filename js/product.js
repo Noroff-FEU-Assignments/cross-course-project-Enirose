3ed33c38fc010c57039794b2b72d30dc66b61b5c
Ma Enirose Hellum <enirosehellum@gmail.com>
2022-08-28 21:37:09 +0200

    fetch products both product and details
    fetch both products. But need to fix the css

--- js/product.js
+++ js/product.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".details");
 const url = "https://rainydays-api.enirosehellum.com/wp-json/wc/store/products/";
 
 async function callApi(){
@@ -7,17 +7,17 @@ async function callApi(){
 
     container.innerHTML="";
 
-       for(let i=0; i<json.length; i++){
+    for(let i=0; i<json.length; i++){
 
-        container.innerHTML +=`<a href="shop.html?id=${json.id}">
-            <div class="container">
-            <img src= "${json[i].images[0].thumbnail}" class="product img"</img>
-            <div><h2>${json[i].name}</h2></div>
-            <div><h3> Price: ${json[i].prices.price}<h3> </div> 
-            </div>
-        </a>`
+        container.innerHTML +=`<a href="shop.html?id=${json[i].id}">
+                                    <div class="details">
+                                        <img src= "${json[i].images[0].thumbnail}"</img>
+                                        <div><h2>${json[i].name}</h2></div>
+                                        <div><h3> Price: ${json[i].price_html}<h3> </div>
+                                    </div>
+                                </a>`;
                 
-        }
+    };
 }
 
 callApi();
--- js/shop.js
+++ js/shop.js
@@ -1,40 +1,29 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-console.log(id)
-
-const container = document.querySelector(".container");
+const container = document.querySelector(".details");
 
 const url = "https://rainydays-api.enirosehellum.com/wp-json/wc/store/products/" +id;
 
-
-
-console.log(url);
+// console.log(url);
 
 async function callApi(){
 
   const response = await fetch(url);
   const json = await response.json();
 
-  const loading = document.querySelector(".loader");
+//     container.innerHTML ="";
 
-  async(function(){
-
-    container.innerHTML ="";
-
-  const html = `
-      <div class = "container">
-      
-        <img src= "${json.images[0].thumbnail}" alt="Image"</img>
+    container.innerHTML += 
+      `<div class = "details">
+        <img src= "${json.images[0].thumbnail}"</img>
         <div><h1> ${json.name}</h1></div>
-        <div><h1> ${json.prices.price}</h1></div>
-
-        <div><h3> ${json.short_description}</h3></div>
+        <div><h1> ${json.attributes.variations}</h1></div>
+        <div><h1> ${json.price_html}</h1></div>
+        <h2> Product Details </h2>
+        <div><span> ${json.short_description}</span></div>
       </div>`
-        
-        container.innerHTML += html;
 
-  })
 }
 
 callApi();
