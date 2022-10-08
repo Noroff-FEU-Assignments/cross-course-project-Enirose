62a474d2a3565094e9f567677d880d3f0d55cad2
Ma Enirose Hellum <enirosehellum@gmail.com>
2022-10-09 01:27:31 +0200

    fixing css

--- js/product.js
+++ js/product.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".details");
+const container = document.querySelector(".row");
 const url = "https://rainydays-api.enirosehellum.com/wp-json/wc/store/products/";
 
 async function callApi(){
@@ -9,13 +9,20 @@ async function callApi(){
 
     for(let i=0; i<json.length; i++){
 
-        container.innerHTML +=`<a href="shop.html?id=${json[i].id}">
-                                    <div class="details">
-                                        <img src= "${json[i].images[0].thumbnail}"</img>
-                                        <div><h2>${json[i].name}</h2></div>
-                                        <div><h3> Price: ${json[i].price_html}<h3> </div>
-                                    </div>
-                                </a>`;
+        container.innerHTML +=
+                        `<div class="product">
+                            <a href="shop.html?id=${json[i].id}"><img src= "${json[i].images[0].thumbnail}"></a>
+                            <div class="star">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <h2 class="p-name">${json[i].name}</h2>
+                            <h3 class="p-price"> Price: ${json[i].price_html}<h3>
+                            <a href="shop.html"><button class="buy-btn">Buy Now</button></a>
+                        </div>`;
                 
     };
 }
