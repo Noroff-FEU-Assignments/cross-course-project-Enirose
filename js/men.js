9896223f5f875cb2a4bbfcdd4381d2d0169098a2
Ma Enirose Hellum <enirosehellum@gmail.com>
2022-10-14 02:18:34 +0200

    some changes with css

--- js/men.js
+++ js/men.js
@@ -1,11 +1,14 @@
 const container = document.querySelector(".row");
-const url = 'https://rainydays-api.enirosehellum.com/wp-json/wc/store/products?categories=17';
+const url = `https://rainydays-api.enirosehellum.com/wp-json/wc/store/products?category=17`;
+const loader = document.querySelector(".loader");
 
 async function callApi(){
     const res = await fetch(url);
     const json = await res.json();
 
     container.innerHTML="";
+
+    loader.classList.remove("loader");
 
     for(let i=0; i<json.length; i++){
 
@@ -21,7 +24,7 @@ async function callApi(){
                             </div>
                             <h2 class="p-name">${json[i].name}</h2>
                             <h3 class="p-price"> Price: ${json[i].price_html}<h3>
-                            <a href="shop.html"><button class="buy-btn">Buy Now</button></a>
+                            <a href="shop.html?id=${json[i].id}"><button class="buy-btn">Buy Now</button></a>
                         </div>`;
                 
     };
--- js/shop.js
+++ js/shop.js
@@ -1,7 +1,7 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-const container = document.querySelector(".details");
+const container = document.querySelector(".s-row");
 
 const url = "https://rainydays-api.enirosehellum.com/wp-json/wc/store/products/" +id;
 
@@ -15,12 +15,18 @@ async function callApi(){
 //     container.innerHTML ="";
 
     container.innerHTML += 
-      `<div class = "details">
-        <img src= "${json.images[0].thumbnail}"</img>
-        <div><h1> ${json.name}</h1></div>
+      `<div class = "s-row">
+        <div><img src= "${json.images[0].thumbnail}"</img></div>
+        <div class = "p-details"><h1> ${json.name}</h1></div>
+        <select>
+          <option>Select size</option>
+          <option value="S">Small</option>
+          <option value="M">Medium</option>
+          <option value="L">Large</option>
+        </select>
         <div><h2> ${json.price_html}</h2></div>
         <a href="cart.html"><button class="buy-btn">Add to Cart</button></a>
-        <h2> Product Details </h2>
+        <h4> Product Details </h4>
         <div><p> ${json.short_description}</p></div>
       </div>`
 
